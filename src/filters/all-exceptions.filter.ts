bbd8a4556316c7e9e2b725c95cd84f2e24e3a4ce
Abhay <chaudhary.abhay@gmail.com>
2022-01-02 22:08:03 +0530

    Initial commit after generating code from https://github.com/Onix-Systems/nest-js-boilerplate

---
+++ src/filters/all-exceptions.filter.ts
@@ -0,0 +1,41 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  NotFoundException,
+  UnauthorizedException,
+  BadRequestException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export default class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+
+    const mongodbCodes = {
+      bulkWriteError: 11000,
+    };
+
+    if (exception.code === mongodbCodes.bulkWriteError) {
+      return res.status(HttpStatus.CONFLICT).json({
+        message:
+          'Any message which should help the user to resolve the conflict',
+      });
+    }
+
+    if (
+      exception instanceof NotFoundException ||
+      exception instanceof UnauthorizedException ||
+      exception instanceof BadRequestException ||
+      exception.code === 'ValidationException'
+    ) {
+      return res.redirect('/v1/auth/sign-up'); // here you can specify rendering your page
+    }
+    return res
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Something is broken' });
+  }
+}
